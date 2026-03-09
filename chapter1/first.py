57bf023b04a9092efcd1683a2ed74a79252472a8
Siddappa Haidri <sidduhsiddu8@gmail.com>
2026-03-09 21:20:51 +0530

    Add files via upload

---
+++ chapter1/first.py
@@ -0,0 +1,22 @@
+#single line 
+"""print("hello world")"""
+#multiple line
+print(''' 
+      twinkle twinkle little star
+      how i wonder what you are
+      up above the world so high
+      like a diamond in the sky 
+      twinkle twinkle little star   
+      how i wonder what you are''')
+
+#modules
+#written by someone else and we can use it in our code
+
+#pip 
+#pip is used to install modules in python
+
+#comments
+#comments are used to explain the code and make it more readable
+#comments are ignored by the interpreter
+#1. single line comment
+#2. multi line comment """ """or ''' '''
